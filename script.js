760c5176650144cec288ca77aacaf8bcb051cbcc
Aldrin <aldrinmcaalim@gmail.com>
2021-03-03 22:47:24 -0500

    403 Forbidden
    After checking the network I found that I have the '403 Forbidden' where the client error status response code indicates that the server understood the request but refuses to authorize it
    
    I will see what I can't do to adjust the project, I just wanted to use an API that didn't have to use keys

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote from API
 async function getQuote() {
+    // The proxyUrl or the apiUrl give me a 403 Forbidden, meaning I don't have access to the information which means I can't use it to make random quotes 
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        // If the author value is empty add "Unknown"
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for longer quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
     } catch (error) {
         getQuote();
-        console.log('whoops, no quote', error);
     }
 }
 
 // On Load
 getQuote();
+
